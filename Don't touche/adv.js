dfac400ecad8badd1aca89445c3a446902b766c5
RoBoCRAFTYT01 <fa83734@gmail.com>
2025-01-06 22:01:39 +0100

    Soon

--- Don't touche/adv.js
+++ Don't touche/adv.js
@@ -1,9 +1,1 @@
-document.querySelectorAll(".btn").forEach((button) => {
-    button.addEventListener("click", () => {
-        const setButtonData = button.getAttribute("data-target");
-        const getDivId = document.getElementById(setButtonData);
-
-        getDivId.style.display =
-        getDivId.style.display === "block" ? "none" : "block";
-    });
-});
+// new update soon
